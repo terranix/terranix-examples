a934aa1cf88f6bd6c6ddb4c77b77ec6e1660bd5e
Ingolf Wagner <contact@ingolf-wagner.de>
2021-11-07 16:50:01 +0100

    remove oder complicated examples

--- flake.nix
+++ flake.nix
@@ -4,30 +4,20 @@
   outputs = { self }: {
 
     templates = {
-      # nix flake init -t github:terranix/terranix-examples#flake
-      flake = {
-        path = ./flake;
-        description = "shows how to use nix flakes and terranix";
-      };
       # nix flake init -t github:terranix/terranix-examples#aws-nixos-server
       aws-nixos-server = {
         path = ./aws-nixos-server;
         description = "deploy an nixos machine as ec2 on aws";
       };
+      # nix flake init -t github:terranix/terranix-examples#flake
+      flake = {
+        path = ./flake;
+        description = "shows how to use nix flakes and terranix";
+      };
       # nix flake init -t github:terranix/terranix-examples#hcloud-nixos-server
       hcloud-nixos-server = {
         path = ./hcloud-nixos-server;
         description = "deploy an nixos machine on hcloud using nixinfect";
-      };
-      # nix flake init -t github:terranix/terranix-examples#hcloud-nixos-server-with-plops
-      hcloud-nixos-server-with-plops = {
-        path = ./hcloud-nixos-server-with-plops;
-        description = "deploy an nixos machine on hcloud using nixinfect and a krops overlay";
-      };
-      # nix flake init -t github:terranix/terranix-examples#hcloud-simple-docker
-      hcloud-simple-docker = {
-        path = ./hcloud-simple-docker;
-        description = "deploy an nixos machine on hcloud using nixinfect and docker";
       };
     };
 
