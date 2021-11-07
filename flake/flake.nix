c7a55caaf68fbff95e65c948dbaf546ec069c6e0
Ingolf Wagner <contact@ingolf-wagner.de>
2021-11-07 10:57:07 +0100

    flake: 2.4.0 -> 2.5.0

--- flake/flake.nix
+++ flake/flake.nix
@@ -13,31 +13,26 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
         terraform = pkgs.terraform_0_15;
-        terraformConfiguration = terranix.lib.buildTerranix {
-          inherit pkgs;
-          terranix_config = {
-            # import a config.nix and maybe other terranix flakes
-            imports = [ ./config.nix ];
-            # and or inline your terranix code
-            resource.local_file.test = {
-              filename = "test.txt";
-              content = "A terranix created test file. YEY!";
-            };
-          };
+        terraformConfiguration = terranix.lib.terranixConfiguration {
+          inherit system;
+          modules = [ ./config.nix ];
         };
-      in {
+      in
+      {
         defaultPackage = terraformConfiguration;
         # nix develop
         devShell = pkgs.mkShell {
-          buildInputs =
-            [ pkgs.terraform_0_15 terranix.defaultPackage.${system} ];
+          buildInputs = [
+            pkgs.terraform_0_15
+            terranix.defaultPackage.${system}
+          ];
         };
         # nix run ".#apply"
         apps.apply = {
           type = "app";
           program = toString (pkgs.writers.writeBash "apply" ''
             if [[ -e config.tf.json ]]; then rm -f config.tf.json; fi
-            cp ${terraformConfiguration}/config.tf.json config.tf.json \
+            cp ${terraformConfiguration} config.tf.json \
               && ${terraform}/bin/terraform init \
               && ${terraform}/bin/terraform apply
           '');
@@ -47,7 +42,7 @@
           type = "app";
           program = toString (pkgs.writers.writeBash "destroy" ''
             if [[ -e config.tf.json ]]; then rm -f config.tf.json; fi
-            cp ${terraformConfiguration}/config.tf.json config.tf.json \
+            cp ${terraformConfiguration} config.tf.json \
               && ${terraform}/bin/terraform init \
               && ${terraform}/bin/terraform destroy
           '');
